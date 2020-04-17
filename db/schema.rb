23a81a4881756409d8008db55b4d41e52ebcb123
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2020-04-18 02:27:22 +0700

    Add model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_191512) do
+ActiveRecord::Schema.define(version: 2020_04_17_192414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 2020_04_17_191512) do
     t.string "title"
     t.string "plot"
     t.integer "number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
