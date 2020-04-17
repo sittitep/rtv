ab2c0aac6cd3bfc7a3059f4efbfa465f2da040f1
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2020-04-18 05:37:17 +0700

    Update CreatePurchaseService to protect purchasing alive contents

--- app/services/create_purchase_service.rb
+++ app/services/create_purchase_service.rb
@@ -4,9 +4,17 @@ class CreatePurchaseService
   end
 
   def call(user_id:, purchase_option_id:)
+    user = User.find(user_id)
+    purchase_option = PurchaseOption.find(purchase_option_id)    
+    user_contents = QueryUserContentService.call(user_id: user.id)
+
+    raise PrevPurchaseIsNotExpired if user_contents.pluck(:id).include?(purchase_option.content.id)
+
     purchase = Purchase.new(user_id: user_id, expired_at: 2.days.from_now)
     purchase.purchase_items.new(purchase_option_id: purchase_option_id)
 
     purchase.save && purchase
   end
 end
+
+class PrevPurchaseIsNotExpired < StandardError; end;
--- spec/services/create_purchase_service_spec.rb
+++ spec/services/create_purchase_service_spec.rb
@@ -2,15 +2,39 @@ require 'rails_helper'
 
 RSpec.describe "CreatePurchaseService" do
   describe "call" do
-    it "returns a saved purchase" do
-      user = create(:user)
-      purchase_option = create(:purchase_option)
+    let(:user) { create(:user) }
+    let(:purchase_option) { create(:purchase_option) }
 
-      purchase = CreatePurchaseService.call(user_id: user.id, purchase_option_id: purchase_option.id)
+    context "new content" do
+      it "returns a saved purchase" do
+        purchase = CreatePurchaseService.call(user_id: user.id, purchase_option_id: purchase_option.id)
 
-      expect(purchase).to be_persisted    
-      expect(purchase.user_id).to eq(user.id)  
-      expect(purchase.purchase_items.first.purchase_option).to eq(purchase_option)  
+        expect(purchase).to be_persisted    
+        expect(purchase.user_id).to eq(user.id)  
+        expect(purchase.purchase_items.first.purchase_option).to eq(purchase_option)  
+      end
+    end
+    
+    context "previous purchase is not expired" do
+      it "must not allow to create purchase and raise error" do
+        create(:purchase, user: user, purchase_items: [build(:purchase_item, purchase_option: purchase_option)])
+  
+        expect { 
+          CreatePurchaseService.call(user_id: user.id, purchase_option_id: purchase_option.id) 
+        }.to raise_error("PrevPurchaseIsNotExpired")
+      end
+    end
+  
+    context "previous purchase is expired" do
+      it "allows to repurchase the content" do
+        create(:purchase, :expired, user: user, purchase_items: [build(:purchase_item, purchase_option: purchase_option)])
+  
+        purchase = CreatePurchaseService.call(user_id: user.id, purchase_option_id: purchase_option.id)
+  
+        expect(purchase).to be_persisted    
+        expect(purchase.user_id).to eq(user.id)  
+        expect(purchase.purchase_items.first.purchase_option).to eq(purchase_option)  
+      end
     end
   end
 end
