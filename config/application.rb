990c425604d8aeb990aad48db19606d263171de6
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2020-04-17 22:43:00 +0700

    Update config generator

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module Rtv
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.stylesheets false
+      g.helper false
+    end
   end
 end
